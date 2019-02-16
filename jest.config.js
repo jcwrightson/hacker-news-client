9278d74f1ce2b50caa03526b68b83c9ddd7b25cb
jcwrightson <jcwrightson@gmail.com>
2019-02-16 11:24:08 +0100

    env setup

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+	moduleFileExtensions: ['js'],
+	transform: {
+		"^.+\\.js$": "babel-jest"
+	},
+	transformIgnorePatterns: ['<rootDir>/node_modules/']
+}
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path');
+
+module.exports = {
+	entry: ['./src/index.js', './src/style.scss'],
+	mode: 'development',
+	module:{
+		rules: [
+			{
+				test: /\.scss$/,
+				exclude: /(node_modules|bower_components)/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					"css-loader",
+					"sass-loader"
+				]
+			},
+			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				exclude: /(node_modules|bower_components)/,
+				options:{
+					"presets": ["@babel/preset-env"],
+					"plugins": [
+						["@babel/plugin-transform-async-to-generator"],
+						// ["@babel/plugin-transform-runtime"]
+					  ]
+				}
+			}	
+		]
+	},
+	devtool: 'inline-source-map',
+	plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+		}),
+		new HtmlWebpackPlugin()
+	],
+	devServer: {
+		contentBase: path.join(__dirname, 'dist'),
+		compress: true,
+		hot: true,
+		port: 8000
+	}
+}
